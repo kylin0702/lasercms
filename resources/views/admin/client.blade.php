f5b932c460f5bd3bf053ed291d0cf0ec90683887
kylin0702 <215627922@qq.com>
2018-06-05 14:48:06 +0800

    some change

--- resources/views/admin/client.blade.php
+++ resources/views/admin/client.blade.php
@@ -46,15 +46,16 @@
         </div><!-- /.box-header -->
         <div class="box-body">
             <div class="row">
-                <div class="col-lg-3"><i class="fa fa-user"></i> 法人名称:{{$v->Owner}}</div>
-                <div class="col-lg-3"><i class="fa fa-mobile"></i> 联系方式:{{$v->Phone}}</div>
-                <div class="col-lg-3"><i class="fa fa-sitemap"></i> 所属区域:{{$v->hasOneArea->AreaName}}</div>
-                <div class="col-lg-3"><i class="fa fa-calendar-check-o"></i>注册时间:{{date("Y-m-d",strtotime($v->UpdateTime))}}</div>
+                <div class="col-lg-2"><i class="fa fa-user"></i> 用户编号:{{$v->ClientNum}}</div>
+                <div class="col-lg-2"><i class="fa fa-user"></i> 法人名称:{{$v->Owner}}</div>
+                <div class="col-lg-2"><i class="fa fa-mobile"></i> 联系方式:{{$v->Phone}}</div>
+                <div class="col-lg-2"><i class="fa fa-sitemap"></i> 所属区域:{{$v->hasOneArea->AreaName}}</div>
+                <div class="col-lg-2"><i class="fa fa-calendar-check-o"></i>注册时间:{{date("Y-m-d",strtotime($v->UpdateTime))}}</div>
+                <div class="col-lg-2"><i class="fa fa-check"></i>审核状态:{{$v->Review}}</div>
             </div>
             <div class="row">
                 <div class="col-lg-6"><i class="fa fa-map-marker"></i> 客户地址:{{$v->Adress}}</div>
-                <div class="col-lg-3"><i class="fa fa-check"></i>审核状态:{{$v->Review}}</div>
-                <div class="col-lg-3">
+                <div class="col-lg-6">
                     <i class="fa fa-cog"></i>操作:
                     <a href="/admin/clients/{{$v->ID}}/edit" class="btn btn-sm btn-info">修改信息</a>
                     <a href="/admin/equipments/create?cid={{$v->ID}}" class="btn btn-sm btn-success">添加光源</a>
