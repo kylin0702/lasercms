bf659d01534071b32e14169f15283a95331e9c8b
kylin0702 <215627922@qq.com>
2018-06-13 11:34:22 +0800

    some change

--- app/Admin/Controllers/AbnormaController.php
+++ app/Admin/Controllers/AbnormaController.php
@@ -107,7 +107,7 @@ EOT;
             });
             $grid->Remark("备注");
             $grid->Serious("严重程度")->display(function($v){
-                $serios=["一般"=>"primary","严重"=>"warning","特别严重"=>"danger"];
+                $serios=["一般"=>"primary","严重"=>"warning","特别严重"=>"danger","否"=>"primary"];
                 return "<Label class='label label-$serios[$v]'>$v</Label>";
             });
             $grid->Solve("是否解决");
